0f22bea44f5db4135f4171e9c79b062f093a3090
João Bispo <joaobispo2077@gmail.com>
2022-11-01 17:20:52 -0300

    feat: use server side rendering to show count

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+
+export type HomeProps = {
+  betPoolCount: number;
+};
+
+export default function Home(props: HomeProps) {
+
   return (
     <h1>
-      Hello world
+      Hello world {props.betPoolCount}
     </h1>
   )
 }
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count')
+  const data = await response.json();
+
+  return {
+    props: {
+      betPoolCount: data.betPoolCount
+    }
+  }
+};
