7221992a77641a4dfacdbb673a3416de20f00f78
João Bispo <joaobispo2077@gmail.com>
2022-11-04 15:14:36 -0300

    feat: add fonts and loading

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider, theme } from 'native-base';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, StatusBar, theme } from 'native-base';
+import { Roboto_400Regular,Roboto_500Medium, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { useFonts } from 'expo-font';
+import { Loading } from './src/components/Loading';
+import { SignInScreen } from './src/screens/SignIn';
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+
   return (
     <NativeBaseProvider theme={theme}>
-        <Text>Open up App.js to start working on your app!! !</Text>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {isFontsLoaded ? <SignInScreen /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,9 @@
+import { Flex, Text } from "native-base";
+
+export const SignInScreen = () => {
+  return (
+    <Flex flex={1} bg="gray.900" justify="center" align="center">
+      <Text color="white" bold>SignInScreen</Text>
+    </Flex>
+  )
+};
